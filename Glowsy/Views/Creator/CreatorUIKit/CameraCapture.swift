49dfe224eddb6baea8884df9b4a883f47dce5f7a
Lazynius1 <scene.am@gmail.com>
2026-05-18 20:38:37 +0200

    Extract creator camera capture

---
+++ Glowsy/Views/Creator/CreatorUIKit/CameraCapture.swift
@@ -0,0 +1,72 @@
+import AVFoundation
+import SwiftUI
+
+struct CameraCapture: UIViewControllerRepresentable {
+    let onCapture: (CreatorMedia) -> Void
+    @Environment(\.dismiss) private var dismiss
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.mediaTypes = ["public.image", "public.movie"]
+        picker.delegate = context.coordinator
+        picker.videoQuality = .typeHigh
+        picker.videoMaximumDuration = 60
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraCapture
+
+        init(_ parent: CameraCapture) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                let detectedRatio = CreatorMedia.AspectRatio.fromRatio(image.size.width / image.size.height)
+                let media = CreatorMedia(
+                    id: UUID().uuidString,
+                    image: image,
+                    videoURL: nil,
+                    type: .image,
+                    aspectRatio: detectedRatio,
+                    recommendedAspectRatio: detectedRatio
+                )
+                parent.onCapture(media)
+            } else if let videoURL = info[.mediaURL] as? URL {
+                let asset = AVAsset(url: videoURL)
+                let generator = AVAssetImageGenerator(asset: asset)
+                generator.appliesPreferredTrackTransform = true
+
+                do {
+                    let cgImage = try generator.copyCGImage(at: .zero, actualTime: nil)
+                    let thumbnail = UIImage(cgImage: cgImage)
+                    let detectedRatio = CreatorMedia.AspectRatio.fromRatio(thumbnail.size.width / thumbnail.size.height)
+                    let media = CreatorMedia(
+                        id: UUID().uuidString,
+                        image: thumbnail,
+                        videoURL: videoURL,
+                        type: .video,
+                        aspectRatio: detectedRatio,
+                        recommendedAspectRatio: detectedRatio
+                    )
+                    parent.onCapture(media)
+                } catch {
+                }
+            }
+
+            parent.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.dismiss()
+        }
+    }
+}
